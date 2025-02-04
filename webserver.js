920347ddda0d81874cada69115a608904f878460
vitalii boiko <vukelan@gmail.com>
2025-02-04 18:56:21 +0200

    add keys

--- webserver.js
+++ webserver.js
@@ -1,1 +1,10 @@
-const express=require('express'),app=express();app.use(express.static('web'));app.listen(3000);
+const fs = require('fs');
+const https = require('https');
+const express = require('express');
+const options = {
+  key: fs.readFileSync('../Ogar/keys/key.pem'),
+  cert: fs.readFileSync('../Ogar/keys/cert.pem'),
+};
+const app = express();
+app.use(express.static('web'));
+https.createServer(options, app).listen(3000);
