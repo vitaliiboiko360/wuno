9fb771821966f9b365a95f3bde536557ad5e5b08
user1 <vukelan@gmail.com>
2025-07-30 20:46:48 +0300

    add main handler

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
+using WsAppFile;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var wsApp = new WsApp();
+
+app.UseWebSockets();
+
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/ws", wsApp.Main);
 
 app.Run();
