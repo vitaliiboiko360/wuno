484d05a75a835fc9de778befd97e94e47ad30666
user1 <vukelan@gmail.com>
2025-08-21 23:56:41 +0300

    add change notification

--- src/table/table_state.cs
+++ src/table/table_state.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using SeatsFile;
 
 namespace TableStateFile;
@@ -8,30 +9,41 @@ interface ITableState
   public void freeSeat(Seat seat);
 }
 
-class TableState : ITableState
+class TableState : ITableState, INotifyPropertyChanged
 {
   bool topSeat = false;
   bool rightSeat = false;
   bool bottomSeat = false;
   bool leftSeat = false;
 
+  public event PropertyChangedEventHandler PropertyChanged;
+
+  protected virtual void OnPropertyChanged(string propertyName)
+  {
+    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+  }
+
   public void allocateSeat(Seat seat)
   {
-    if (seat == Seat.Bottom)
+    if (seat == Seat.Bottom && !bottomSeat)
     {
       bottomSeat = true;
+      OnPropertyChanged(nameof(bottomSeat));
     }
-    if (seat == Seat.Left)
+    if (seat == Seat.Left && !leftSeat)
     {
       leftSeat = true;
+      OnPropertyChanged(nameof(leftSeat));
     }
-    if (seat == Seat.Top)
+    if (seat == Seat.Top && !topSeat)
     {
       topSeat = true;
+      OnPropertyChanged(nameof(topSeat));
     }
-    if (seat == Seat.Right)
+    if (seat == Seat.Right && !rightSeat)
     {
       rightSeat = true;
+      OnPropertyChanged(nameof(rightSeat));
     }
   }
 
