6245c03983b2ae151a3b95cd00bc7e55577c922f
user1 <vukelan@gmail.com>
2025-08-23 22:04:46 +0300

    add aprove seat method

--- src/table_game_manager.cs
+++ src/table_game_manager.cs
@@ -1,3 +1,4 @@
+using System.Net.WebSockets;
 using ManagerCommandsFile;
 using SeatsFile;
 using TableStateFile;
@@ -13,10 +14,12 @@ public interface ITableGameManager
 public class TableGameManager : ITableGameManager
 {
   ITableState _tableState;
+  IWsConnections _wsConnections;
 
-  TableGameManager(ITableState tableState)
+  TableGameManager(ITableState tableState, IWsConnections wsConnections)
   {
     _tableState = tableState;
+    _wsConnections = wsConnections;
   }
 
   public void ProcessMessage(WsMessage wsMessage)
@@ -45,27 +48,60 @@ public class TableGameManager : ITableGameManager
   void ProcessTableMessage(WsMessage wsMessage)
   {
     uint actByte = wsMessage.buffer[1];
-    if (actByte == (uint)wsMessage.buffer[1])
+    if (actByte == (uint)TableActionsIncoming.RequestSeat)
     {
       uint place = wsMessage.buffer[2];
       if (place == (uint)Seat.Left)
       {
-        _tableState.allocateSeat(Seat.Left);
+        if (_tableState.allocateSeat(Seat.Left))
+        {
+          approveSeatRequest(wsMessage.ws, place);
+        }
       }
       if (place == (uint)Seat.Right)
       {
-        _tableState.allocateSeat(Seat.Right);
+        if (_tableState.allocateSeat(Seat.Right))
+        {
+          approveSeatRequest(wsMessage.ws, place);
+        }
       }
       if (place == (uint)Seat.Top)
       {
-        _tableState.allocateSeat(Seat.Top);
+        if (_tableState.allocateSeat(Seat.Top))
+        {
+          approveSeatRequest(wsMessage.ws, place);
+        }
       }
       if (place == (uint)Seat.Bottom)
       {
-        _tableState.allocateSeat(Seat.Bottom);
+        if (_tableState.allocateSeat(Seat.Bottom))
+        {
+          approveSeatRequest(wsMessage.ws, place);
+        }
       }
     }
   }
 
   void ProcessGameMessage(WsMessage wsMessage) { }
+
+  void approveSeatRequest(WebSocket webSocket, uint seat)
+  {
+    var arrayToSend = new byte[8];
+    arrayToSend[0] = (byte)ManagerCommands.Table;
+    arrayToSend[1] = (byte)TableActionsOutcoming.GrantSeat;
+    webSocket.SendAsync(arrayToSend, WebSocketMessageType.Binary, true, CancellationToken.None);
+    Array.Clear(arrayToSend, 0, arrayToSend.Length);
+    arrayToSend[0] = (byte)ManagerCommands.Table;
+    arrayToSend[1] = (byte)TableActionsOutcoming.TakeSeat;
+    arrayToSend[2] = (byte)seat;
+    for (var i = 0; i < _wsConnections.GetConnections().Count; i++)
+    {
+      var ws = _wsConnections.GetConnections()[i];
+      if (ws == webSocket)
+      {
+        continue;
+      }
+      ws.SendAsync(arrayToSend, WebSocketMessageType.Binary, true, CancellationToken.None);
+    }
+  }
 }
